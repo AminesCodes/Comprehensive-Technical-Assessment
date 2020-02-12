efcc8e07ae315f63f2db51f4fba8fb7b64d40f4e
AminesCodes <aminebensalem@pursuit.org>
2020-02-12 03:01:55 -0500

    Frontend - Home component using About

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
+
 import LoginForm from './LoginForm'
+import About from './About'
 
 export default function Home (props) {
-    const imageStyle = {
-        width: '80%', 
-        height: '80%', 
-        display: 'block', 
-        objectFit: 'scale-down',
-        margin: 'auto'
-    }
-
     const loggedUserId = localStorage.getItem('#TV#$how@Watch&List#_UID') 
     let form = null
     if (!loggedUserId) {
@@ -23,12 +17,13 @@ export default function Home (props) {
     return(
         <>
             {form}
+            <h2 className='text-center m-4'>Welcome to TV Show WatchList App</h2>
             <img 
+                className='d-block w-75 mx-auto'
                 src={require('../assets/Best-TV-Shows.jpg')} 
                 alt='Multiple TV Shows images in one pic' 
-                style={imageStyle}
             /> 
-            <h2 className='text-center'>Welcome to TV Show WatchList App</h2>
+            <About />
         </>
     )
 }
