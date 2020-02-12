2f1f2561f54af36c9e94deb3ef57370505974996
AminesCodes <aminebensalem@pursuit.org>
2020-02-12 02:55:26 -0500

    Frontend - About component

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 export default function About (props) {
 
     return(
-        <>
-            About
-        </>
+        <div className='container mt-5'>
+            <h2 className='text-center'>About</h2>
+            <h3 className='text-center m-5'>TV Show WatchList App</h3>
+            <p>TV Show WatchList is an app designed to helps people decide what to watch next, 
+                mostly by reading comments on a selected show or just by referring to how many users 
+                have watched it.
+            </p>
+            <br />
+            <br />
+            <br />
+            {/* <h5 className='float-left mt-3'></h5> */}
+            <h6 className='float-right mt-3 text-right'>Developed by Amine</h6>
+        </div>
     )
 }
