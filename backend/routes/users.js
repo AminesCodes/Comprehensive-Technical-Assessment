edc1c811bb96e8bd293d73b67562bb9684e929af
AminesCodes <aminebensalem@pursuit.org>
2020-02-12 23:32:24 -0500

    Backend - User Authentication completed

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -43,26 +43,6 @@ router.get('/:userId', async (request, response) => {
   }
 });
 
-/* POST: create a new user. */
-router.post('/', async (request, response) => {
-  const { username, avatar_url } = request.body
-  if (paramChecker(response, username) 
-      && paramChecker(response, avatar_url) 
-      && !idChecker(response, username) // Making sure a username is not all numbers
-    ) {
-    try {
-      const user = await usersQuery.createUser(username, avatar_url)
-      response.json({
-        error: false,
-        message: 'Successfully created a new user',
-        payload: user
-      })
-    } catch (err) {
-      sendError(response, err)
-    }
-  }
-});
-
 
 
 module.exports = router;
